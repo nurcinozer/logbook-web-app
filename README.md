3b94f696496c697de55b11d5ef2036864d01bf55
Nurçin <nrcinozer@gmail.com>
2022-12-11 21:01:21 +0300

    feat: added tmdb api url and key

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Web app to log your movies, books and games.
 
 :warning: This project is in implementation planning phase.
 
-## Planned tech stack
+## Planned tech stack
 
 - [x] [Create T3 App](https://create.t3.gg/)
 - [x] [Supabase](https://supabase.io/)
+- [ ] [TMDB API](https://developers.themoviedb.org/3/getting-started/introduction)
 - [ ] [Mantine Dev UI](https://mantine.dev/)
-- [ ] [Zustand](https://github.com/pmndrs/zustand)
--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -17,6 +17,9 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+  NEXT_PUBLIC_TMDB_API_KEY: z.string(),
 });
 
 /**
@@ -27,4 +30,7 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
 };
