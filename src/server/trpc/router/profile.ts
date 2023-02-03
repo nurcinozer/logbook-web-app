1cb81652f6ab4fa517c429dd1815197d0f70d6a7
Nurçin <nrcinozer@gmail.com>
2023-02-03 20:27:42 +0300

    refactor: removed unnecessary files

--- src/server/trpc/router/profile.ts
+++ src/server/trpc/router/profile.ts
@@ -4,7 +4,7 @@ export const profileRouter = router({
   profileBySession: protectedProcedure.query(async ({ ctx }) => {
     const user = await ctx.prisma.user.findFirst({
       where: {
-        id: ctx.session?.user?.id,
+        id: ctx.session.user.id,
       },
       include: {
         profile: true,
--- src/types/context.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export type Context = {
-  res: NextApiResponse;
-  req: NextApiRequest;
-};
